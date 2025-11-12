b2b638e5e5203db4efed0a95b70337e6ce4888b3
fioritog <gfiorito@frsn.utn.edu.ar>
2025-11-12 15:00:24 -0300

    controller y service actualizado para historial de reserva

--- app/src/pages/user-reservations.jsx
+++ app/src/pages/user-reservations.jsx
@@ -5,7 +5,7 @@ export default function UserReservations() {
   return (
     <>
       <Header />
-      <main></main>
+      <main className="flex flex-col items-center gap-10 md:gap-16 my-8"></main>
       <Footer />
     </>
   );
